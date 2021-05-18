1a1061aa92f3cd622192923733c43860398f0d41
Kathiravan Kumarasamy <kathiravan.kumarasamy@adelaide.edu.au>
2021-05-18 23:29:57 +0530

    updated nuxt configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,64 +1,83 @@
 export default {
-    components: true,
-    head: {
-        titleTemplate: "Mastering Nuxt: %s",
-        htmlAttrs: {
-            lang: "en"
-        },
-        bodyAttrs:{
-            class: ["my-style"]
-        },
-        meta: [{
-            charset: "utf-8",
-        }]
+  components: true,
+  server: {
+    // port: 3100, // default 3000
+    port: process.env.PORT || 3600,
+  },
+  /*
+   ** Disable Nuxt asking for participation
+   */
+  telemetry: false,
+  head: {
+    titleTemplate: "Mastering Nuxt: %s",
+    htmlAttrs: {
+      lang: "en",
     },
-    router: {
-        prefetchLinks: false,
+    bodyAttrs: {
+      class: ["my-style"],
     },
-    plugins:[ 
-        '~/plugins/maps.client', 
-        '~/plugins/dataApi', 
-        '~/plugins/auth.client', 
-        '~/plugins/vCalendar.client'
+    meta: [
+      {
+        charset: "utf-8",
+      },
     ],
-    modules:['~/modules/auth', '~/modules/algolia', '~/modules/cloudinary', '@nuxtjs/cloudinary'],
-    buildModules:['@nuxtjs/tailwindcss', '@nuxt/image'],
-    cloudinary:{
-        cloudName: 'dzxztzdg4',
+  },
+  router: {
+    prefetchLinks: false,
+  },
+  plugins: [
+    "~/plugins/maps.client",
+    "~/plugins/dataApi",
+    "~/plugins/auth.client",
+    "~/plugins/vCalendar.client",
+  ],
+  modules: [
+    "~/modules/auth",
+    "~/modules/algolia",
+    "~/modules/cloudinary",
+    "@nuxtjs/cloudinary",
+  ],
+  buildModules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
+  cloudinary: {
+    cloudName: "nuxt-kathir-bnb",
+  },
+  image: {
+    cloudinary: {
+      baseURL: "https://res.cloudinary.com/nuxt-kathir-bnb/image/upload/",
     },
-    image: {
-        cloudinary: {
-          baseURL: 'https://res.cloudinary.com/dzxztzdg4/image/upload/'
-        }
+  },
+  css: ["~/assets/sass/app.scss"],
+  build: {
+    extractCSS: true,
+    loaders: {
+      limit: 0,
     },
-    css: ['~/assets/sass/app.scss'],
-    build: {
-        extractCSS: true,
-        loaders: {
-            limit: 0,
-        }
+    babel: {
+      plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
     },
-    publicRuntimeConfig:{
-        auth:{
-            cookieName: 'idToken',
-            clientId: '537712074419-0pao3sshf84cqqtaie3aumvmobmlseji.apps.googleusercontent.com',
-        },
-        algolia:{
-            appId: 'QXGX8RCAML',
-            key:'f0ecada6cb5d32e6496eb1e42c3d7bbd',
-        },
-        cloudinary:{
-            apiKey: '751854148899179',
-        }
+  },
+  publicRuntimeConfig: {
+    auth: {
+      cookieName: "idToken",
+      clientId:
+        "952132017083-to68rg93dp9ao68ulhim23853vhc3hvq.apps.googleusercontent.com",
     },
-    privateRuntimeConfig:{
-        algolia:{
-            appId: 'QXGX8RCAML',
-            key: '739175c49753db77b422766b417678b1',
-        },
-        cloudinary:{
-            apiSecret: 'Hner5MurgYTJLMQQ9F36P8msRN0',
-        }
+    algolia: {
+      appId: "CRH6AJ5VNU",
+      key: "ee31a7ef16046078ce766f08d1478e5e",
     },
-
-}
+    cloudinary: {
+      apiKey: "418971391689854",
+    },
+  },
+  privateRuntimeConfig: {
+    algolia: {
+      appId: "CRH6AJ5VNU",
+      key: "ee31a7ef16046078ce766f08d1478e5e",
+    },
+    cloudinary: {
+      apiKey: "418971391689854",
+      apiSecret: "WJ5s6YQQEyIg9ZdHAiwohbLuP3U",
+    },
+  },
+};
